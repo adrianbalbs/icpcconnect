347b5964be9268677d4361a0d452b6b448cb2b1a
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-10-04 10:07:34 +1000

    remove old schemas that are clashing

--- backend/drizzle/0001_calm_stranger.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "coaches" ADD PRIMARY KEY ("id");--> statement-breakpoint
-ALTER TABLE "site_coordinators" ADD PRIMARY KEY ("id");--> statement-breakpoint
-ALTER TABLE "students" ADD PRIMARY KEY ("id");
