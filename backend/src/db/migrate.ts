0656bbba9b7df60c9dcf597eeb189cad3a96543f
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-10-06 10:56:16 +1100

    remove top level await from migrate

--- backend/src/db/migrate.ts
+++ backend/src/db/migrate.ts
@@ -1,6 +1,11 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { Database } from "./database.js";
+import { getLogger } from "../utils/logger.js";
 
-await migrate(Database.getConnection(), { migrationsFolder: "./drizzle" });
+export async function runMigrations() {
+  const logger = getLogger();
+  logger.info("Running migrations...");
 
-await Database.endConnection();
+  await migrate(Database.getConnection(), { migrationsFolder: "./drizzle" });
+  logger.info("Migrations completed.");
+}
