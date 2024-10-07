72a3e55826994ef7624d70775cdabbf481961569
Kob <kobeshen123@gmail.com>
2024-10-07 11:19:33 +1100

    added simple stubs for login page

---
+++ backend/src/auth/auth.ts
@@ -0,0 +1,20 @@
+/**
+ * Function for registering a new user
+ */
+export function authRegister(email: String, password: String, firstName: String, lastName: String, role: Number) {
+
+}
+
+/**
+ * Function for an existing user to log in
+ */
+export function authLogin(email: String, password: String) {
+
+}
+
+/**
+ * Function for an existing user to log out
+ */
+export function authLogout(token: String) {
+
+}
