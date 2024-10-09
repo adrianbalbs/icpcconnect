838f1e9deef8947e4058e1e81c750a8de45b94b3
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-10-09 13:01:43 +1100

    feat: F15ATOMATO-59 implement contest registration service

---
+++ backend/src/services/contest-registration-service.ts
@@ -0,0 +1,187 @@
+import {
+  Course,
+  coursesCompletedByStudent,
+  DatabaseConnection,
+  languagesSpokenByStudent,
+  registrationDetails,
+  SpokenLanguage,
+} from "../db/index.js";
+import {
+  CreateContestRegistrationForm,
+  LanguageExperience,
+  Level,
+  UpdateContestRegistrationForm,
+} from "../schemas/index.js";
+import { DeleteResponse } from "../interfaces/index.js";
+import { badRequest, HTTPError, notFoundError } from "../utils/errors.js";
+import { eq } from "drizzle-orm";
+
+export interface CreateRegistrationFormResponse {
+  studentId: string;
+}
+
+export interface GetRegistrationFormResponse {
+  student: string;
+  level: Level;
+  contestExperience: number;
+  leetcodeRating: number;
+  codeforcesRating: number;
+  allergies: string;
+  photoConsent: boolean;
+  cppExperience: LanguageExperience;
+  cExperience: LanguageExperience;
+  javaExperience: LanguageExperience;
+  pythonExperience: LanguageExperience;
+  timeSubmitted: Date;
+  languagesSpoken: SpokenLanguage[];
+  coursesCompleted: Course[];
+}
+
+export type UpdateContestRegistrationFormResponse =
+  UpdateContestRegistrationForm;
+
+export class ContestRegistrationService {
+  private readonly db: DatabaseConnection;
+
+  constructor(db: DatabaseConnection) {
+    this.db = db;
+  }
+
+  async createRegistration(
+    newRegistrationDetails: CreateContestRegistrationForm,
+  ): Promise<CreateRegistrationFormResponse> {
+    const {
+      allergies,
+      cExperience,
+      codeforcesRating,
+      contestExperience,
+      coursesTaken,
+      cppExperience,
+      javaExperience,
+      level,
+      leetcodeRating,
+      photoConsent,
+      pythonExperience,
+      spokenLanguages,
+      student,
+    } = newRegistrationDetails;
+
+    const [{ studentId }] = await this.db
+      .insert(registrationDetails)
+      .values({
+        student,
+        allergies,
+        contestExperience,
+        codeforcesRating,
+        leetcodeRating,
+        photoConsent,
+        level,
+        cExperience,
+        cppExperience,
+        javaExperience,
+        pythonExperience,
+      })
+      .returning({ studentId: registrationDetails.student });
+
+    for (const languageCode of spokenLanguages) {
+      await this.db
+        .insert(languagesSpokenByStudent)
+        .values({ studentId, languageCode });
+    }
+
+    for (const courseId of coursesTaken) {
+      await this.db
+        .insert(coursesCompletedByStudent)
+        .values({ studentId, courseId });
+    }
+
+    return { studentId };
+  }
+
+  async getRegistration(
+    studentId: string,
+  ): Promise<GetRegistrationFormResponse> {
+    const result = await this.db.query.registrationDetails.findFirst({
+      where: (registrationDetails, { eq }) =>
+        eq(registrationDetails.student, studentId),
+      with: {
+        languagesSpoken: true,
+        coursesCompleted: true,
+      },
+    });
+
+    if (!result) {
+      throw new HTTPError(notFoundError);
+    }
+    return result;
+  }
+
+  async updateRegistration(
+    studentId: string,
+    updatedDetails: UpdateContestRegistrationForm,
+  ): Promise<UpdateContestRegistrationFormResponse> {
+    const {
+      allergies,
+      cExperience,
+      codeforcesRating,
+      contestExperience,
+      coursesTaken,
+      cppExperience,
+      javaExperience,
+      leetcodeRating,
+      level,
+      photoConsent,
+      pythonExperience,
+      spokenLanguages,
+    } = updatedDetails;
+
+    await this.db
+      .update(registrationDetails)
+      .set({
+        allergies,
+        contestExperience,
+        codeforcesRating,
+        leetcodeRating,
+        photoConsent,
+        level,
+        cExperience,
+        cppExperience,
+        javaExperience,
+        pythonExperience,
+      })
+      .where(eq(registrationDetails.student, studentId));
+
+    await this.db
+      .delete(languagesSpokenByStudent)
+      .where(eq(languagesSpokenByStudent.studentId, studentId));
+    await this.db
+      .delete(coursesCompletedByStudent)
+      .where(eq(coursesCompletedByStudent.studentId, studentId));
+
+    for (const languageCode of spokenLanguages) {
+      await this.db
+        .insert(languagesSpokenByStudent)
+        .values({ studentId, languageCode });
+    }
+    for (const courseId of coursesTaken) {
+      await this.db
+        .insert(coursesCompletedByStudent)
+        .values({ studentId, courseId });
+    }
+    return updatedDetails;
+  }
+
+  async deleteRegistration(studentId: string): Promise<DeleteResponse> {
+    const [res] = await this.db
+      .select()
+      .from(registrationDetails)
+      .where(eq(registrationDetails.student, studentId));
+    if (!res) {
+      throw new HTTPError(badRequest);
+    }
+    await this.db
+      .delete(registrationDetails)
+      .where(eq(registrationDetails.student, studentId));
+    return { status: "OK" };
+  }
+}
