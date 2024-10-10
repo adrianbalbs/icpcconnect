30252d9f2766a99243b09d8e1f5b18de685d589a
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-10-10 21:21:43 +1100

    fix: add types for frontend api calls

--- frontend/src/app/components/members/Students.tsx
+++ frontend/src/app/components/members/Students.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { SERVER_URL } from '@/utils/constants';
-import pageStyles from '@/styles/Page.module.css';
-import memberStyles from '@/styles/Members.module.css';
-import Student, { StudentProps } from './Student';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SERVER_URL } from "@/utils/constants";
+import pageStyles from "@/styles/Page.module.css";
+import memberStyles from "@/styles/Members.module.css";
+import Student, { StudentProps } from "./Student";
 
 export interface StudentInfo {
   id: string;
@@ -22,59 +22,65 @@ export interface StudentInfo {
 const Students: React.FC = () => {
   const [students, setStudents] = useState<StudentProps[]>([
     {
-      id: '123',
-      name: 'Rachel Chen',
-      team: 'Randos',
-      institution: 'UNSW',
-      email: 'asdlakds'
+      id: "123",
+      name: "Rachel Chen",
+      team: "Randos",
+      institution: "UNSW",
+      email: "asdlakds",
     },
     {
-      id: '123',
-      name: 'Rachel Chen',
-      team: 'Randos',
-      institution: 'UNSW',
-      email: 'asdlakds'
-    }
+      id: "123",
+      name: "Rachel Chen",
+      team: "Randos",
+      institution: "UNSW",
+      email: "asdlakds",
+    },
   ]);
 
   const getStudents = async () => {
     try {
-      const res = await axios.get(`${SERVER_URL}/api/students`);
+      const res = await axios.get<{ allStudents: StudentInfo[] }>(
+        `${SERVER_URL}/api/students`,
+      );
       const allStudents: StudentInfo[] = res.data.allStudents;
-      const filteredInfo: StudentProps[]  = allStudents.map(student => ({
+      const filteredInfo: StudentProps[] = allStudents.map((student) => ({
         id: student.id,
-        name: student.givenName + ' ' + student.familyName,
+        name: student.givenName + " " + student.familyName,
         team: student.team,
         institution: student.university,
         email: student.email,
       }));
       setStudents(filteredInfo);
     } catch (error) {
-        alert(`Get students: ${error}`);
+      alert(`Get students: ${error}`);
     }
-  }
+  };
 
   useEffect(() => {
     getStudents();
   }, []);
 
-  return <div className={memberStyles.gap}>
-    <div className={`${pageStyles.bold} ${memberStyles.students}`}>
-      <p>Student</p>
-      <p>Team Name</p>
-      <p>Institution</p>
-      <p>Email</p>
-    </div>
-    <hr className={pageStyles.divider}/>
-    {students.map(student => <Student key={student.id} {...student} />)}
-    {/* <div className={`${memberStyles.students} ${memberStyles.space}`}>
+  return (
+    <div className={memberStyles.gap}>
+      <div className={`${pageStyles.bold} ${memberStyles.students}`}>
+        <p>Student</p>
+        <p>Team Name</p>
+        <p>Institution</p>
+        <p>Email</p>
+      </div>
+      <hr className={pageStyles.divider} />
+      {students.map((student) => (
+        <Student key={student.id} {...student} />
+      ))}
+      {/* <div className={`${memberStyles.students} ${memberStyles.space}`}>
       <p>Rachel Chen</p>
       <p>Randos</p>
       <p>UNSW Sydney</p>
       <p className={memberStyles.overflow}>z5432123@ad.unsw.edu.au</p>
     </div>
     <hr className={pageStyles.divider}/> */}
-  </div>
-}
+    </div>
+  );
+};
 
 export default Students;
