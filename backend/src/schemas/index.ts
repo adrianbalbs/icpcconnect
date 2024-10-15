b8c5db46b4da9db18e5a0cad68a6945a69c83ed7
Jerry M Yang <jmax.yang@gmail.com>
2024-10-15 13:55:33 +1100

    F15ATOMATO-11 Implemented Stub Routes, Service, and Schema

--- backend/src/schemas/index.ts
+++ backend/src/schemas/index.ts
@@ -2,3 +2,4 @@ export * from "./user-schema.js";
 export * from "./team-schema.js";
 export * from "./codes-schema.js";
 export * from "./contest-registration-schema.js";
+export * from "./teams-schema.js"
