6b0562d25c835e3135afccba58347b3a53fbef06
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-10-20 22:22:59 +1100

    chore: update package json test scripts to do pre and post commands

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,60 +1,80 @@
-'use client'
-
-import Image from 'next/image';
-import axios from 'axios';
-import styles from '@/styles/Auth.module.css';
-import logo from '@/assets/logo.png';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { SERVER_URL } from '@/utils/constants';
-
+"use client";
+import loginPage from "../styles/Auth.module.css";
+import Image from "next/image";
+import logo from "../assets/logo.png";
+import axios from "axios";
+import { SERVER_URL } from "../utils/constants";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
-
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async () => {
     try {
-      e.preventDefault();
-  
       const payload = {
         email,
         password,
-      }
-      const res = await axios.post(`${SERVER_URL}/api/login`, payload);
-      const { token, id, role } = res.data;
-      localStorage.setItem('token', token);
-      localStorage.setItem('id', id);
-      localStorage.setItem('role', role);
-      router.push(role === 'student' ? 'team' : 'teams');
+      };
+      const res = await axios.post(`${SERVER_URL}/api/login`, payload, {
+        withCredentials: true,
+      });
+      console.log(res);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("role", res.data.role);
+      localStorage.setItem("id", res.data.id);
+      router.push("/teams");
     } catch (error) {
-      alert(`Login error: ${error}`);
+      alert(error);
     }
-  }
-    
+  };
+
   return (
-    <div className={styles.background}>
-      <div className={styles.shadow}><div className={styles['login-polygon']}></div></div>
-      <div className={styles['info-container']}>
-        <Image src={logo} alt="ICPC Connect" width={400} className={styles['web-name']}/>
-        <br/>
-        <form className={styles['form-container']} onSubmit={handleLogin}>
-          <input type="email" id="email" placeholder="Email" className={styles['input-field']} value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" id="password" placeholder="Password" className={styles['input-field']} value={password} onChange={(e) => setPassword(e.target.value)} />
-          <a href="/forgot-password" className={styles.link}>Forgot Password?</a>
-          <button type="submit" className={`${styles['auth-button']} ${styles['dark']} ${styles['long']}`}>Login</button>
-        </form>
-        <div className={styles['horizontal-container']}>
-          <hr className={styles.hr}/>
-          or
-          <hr className={styles.hr}/>
+    <>
+      <div className={loginPage["login-polygon"]}></div>
+      <div className={loginPage["info-container"]}>
+        <Image src={logo} alt="" width={400} />
+        <br />
+        <div className={loginPage["form-container"]}>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            className={loginPage["input-field"]}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className={loginPage["input-field"]}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <a href="/forgot-password" className={loginPage.link}>
+            Forgot Password?
+          </a>
         </div>
-        <div className={styles['horizontal-container']}>
+        <button
+          onClick={handleLogin}
+          className={`${loginPage["auth-button"]} ${loginPage["dark"]} ${loginPage["long"]}`}
+        >
+          Login
+        </button>
+        <div className={loginPage["horizontal-container"]}>
+          <hr></hr>
+          or
+          <hr></hr>
+        </div>
+        <div className={loginPage["horizontal-container"]}>
           Don&apos;t have an account?&nbsp;
-          <a href="register" className={styles.link}>Register</a>
+          <a href="register" className={loginPage.link}>
+            Register
+          </a>
         </div>
       </div>
-    </div>
+    </>
   );
 }
