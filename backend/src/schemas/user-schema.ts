d14640ff5266f79776a28c78c226fef90c9407a9
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-11-04 16:33:02 +1100

    refactor: remove redundant schemas

--- backend/src/schemas/user-schema.ts
+++ backend/src/schemas/user-schema.ts
@@ -212,95 +212,12 @@ export const UserRoleEnum = z.enum([
 ]);
 export type UserRole = z.infer<typeof UserRoleEnum>;
 
-export const CreateAdminRequestSchema = z.object({
-  givenName: z.string().min(1).max(35),
-  familyName: z.string().min(1).max(35),
-  password: z.string().min(1).max(128),
-  email: z.string().email(),
-  role: UserRoleEnum.refine((val) => val === "Admin", {
-    message: "Role must be admin",
-  }),
-});
-
-export type CreateAdminRequest = z.infer<typeof CreateAdminRequestSchema>;
-
-export type UpdateAdminRequest = z.infer<typeof CreateAdminRequestSchema>;
-
-export const CreateStudentRequestSchema = z.object({
-  givenName: z.string().min(1).max(35),
-  familyName: z.string().min(1).max(35),
-  password: z.string().min(1).max(128),
-  email: z.string().email(),
-  role: UserRoleEnum,
-  studentId: z.string().min(1),
-  university: z.number(),
-  verificationCode: z.string(),
-  photoConsent: z.boolean(),
-  languagesSpoken: z.array(SpokenLanguageEnum),
-});
-
-export type CreateStudentRequest = z.infer<typeof CreateStudentRequestSchema>;
-
-export const UpdateStudentRequestSchema = CreateStudentRequestSchema.omit({
-  verificationCode: true,
-})
-  .extend({
-    university: z.number(),
-    pronouns: z.string(),
-    team: z.string().nullable(),
-    dietaryRequirements: z.string().nullable(),
-    tshirtSize: z.string(), // Thinking "M", "L", etc. Could do it by numbers? Seems less descriptive
-    photoConsent: z.boolean(),
-  })
-  .partial();
-
-export type UpdateStudentRequest = z.infer<typeof UpdateStudentRequestSchema>;
-
 export const UpdateStudentExclusionsRequestSchema = z.object({
   exclusions: z.string(),
 });
 
 export type UpdateStudentExclusionsRequest = z.infer<
   typeof UpdateStudentExclusionsRequestSchema
->;
-
-export const CreateCoachRequestSchema = z.object({
-  givenName: z.string().min(1).max(35),
-  familyName: z.string().min(1).max(35),
-  password: z.string().min(1).max(128),
-  email: z.string().email(),
-  role: UserRoleEnum,
-  university: z.number(),
-  verificationCode: z.string(),
-});
-
-export type CreateCoachRequest = z.infer<typeof CreateCoachRequestSchema>;
-
-export const UpdateCoachRequestSchema = CreateCoachRequestSchema.omit({
-  verificationCode: true,
-}).partial();
-
-export type UpdateCoachRequest = z.infer<typeof UpdateCoachRequestSchema>;
-
-export const CreateSiteCoordinatorRequestSchema = z.object({
-  givenName: z.string().min(1).max(35),
-  familyName: z.string().min(1).max(35),
-  password: z.string().min(1).max(128),
-  email: z.string().email(),
-  role: UserRoleEnum,
-  university: z.number(),
-  verificationCode: z.string(),
-});
-
-export type CreateSiteCoordinatorRequest = z.infer<
-  typeof CreateSiteCoordinatorRequestSchema
->;
-
-export const UpdateSiteCoordinatorRequestSchema =
-  CreateSiteCoordinatorRequestSchema.omit({ verificationCode: true }).partial();
-
-export type UpdateSiteCoordinatorRequest = z.infer<
-  typeof UpdateSiteCoordinatorRequestSchema
 >;
 
 export const LoginRequestSchema = z.object({
