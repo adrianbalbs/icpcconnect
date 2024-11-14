2618d47815d086ca88f348043c46f600ae43ea11
Yian Li <yian.li1@student.unsw.edu.au>
2024-11-14 23:51:53 +1100

    added change password modal

--- frontend/src/app/profile/[id]/account-settings/page.tsx
+++ frontend/src/app/profile/[id]/account-settings/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-// import { useEffect, useState } from 'react';
+import { useState } from "react";
+import { addBtn } from "@/styles/sxStyles";
 import profileStyles from "@/styles/Profile.module.css";
-import pageStyles from "@/styles/Page.module.css";
-import Info from "@/components/profile/Info";
+import experienceStyles from "@/styles/Experience.module.css";
+import { Button } from "@mui/material";
+import ChangePasswordModal from "@/components/auth/ChangePasswordModal";
 // import { ProfileProps } from '../page';
 
 const AccountSettings: React.FC = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className={profileStyles["inner-screen"]}>
       <div className={profileStyles.title}>
         <h3>Account Settings</h3>
       </div>
-      <hr className={pageStyles.divider} />
-      <Info name="Password" value="yay" />
+      <hr className={experienceStyles.divider} />
+      {!open ? (
+        <Button variant="contained" sx={addBtn} onClick={() => setOpen(true)}>
+          Change Password
+        </Button>
+      ) : (
+        <ChangePasswordModal setOpen={setOpen} />
+      )}
     </div>
   );
 };
