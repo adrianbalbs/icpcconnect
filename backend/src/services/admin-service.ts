66aa6d60930853837da4f3abd247c57759f67764
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-11-17 18:58:39 +1100

    chore: remove dead code

--- backend/src/services/admin-service.ts
+++ backend/src/services/admin-service.ts
@@ -19,44 +19,15 @@ export class AdminService {
   }
 
   /*
-  * Returns admin profile, given an internal user-id
-  *
-  * @param userId - Internal user-id of the admin 
-  * 
-  * @returns The admin profile of the user specified
-  * 
-  * @throws BadRequest if user-id doesn't correspond to an admin
-  * 
-  */
-  async getAdminById(userId: string): Promise<AdminProfileResponse> {
-    const admin = await this.db
-      .select({
-        id: users.id,
-        givenName: users.givenName,
-        familyName: users.familyName,
-        email: users.email,
-        role: users.role,
-      })
-      .from(users)
-      .where(eq(users.id, userId));
-
-    if (!admin.length) {
-      throw new HTTPError(badRequest);
-    }
-
-    return admin[0];
-  }
-
-  /*
-  * Deletes an admin, given their internal user-id
-  *
-  * @param userId - Internal user-id of the admin 
-  * 
-  * @returns DeleteResponse - with an OK status
-  * 
-  * @throws BadRequest if user-id doesn't correspond to an admin
-  * 
-  */
+   * Deletes an admin, given their internal user-id
+   *
+   * @param userId - Internal user-id of the admin
+   *
+   * @returns DeleteResponse - with an OK status
+   *
+   * @throws BadRequest if user-id doesn't correspond to an admin
+   *
+   */
   async deleteAdmin(userId: string): Promise<DeleteResponse> {
     const admin = await this.db
       .select({ userId: users.id })
